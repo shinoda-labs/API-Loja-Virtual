6380b7f5ea94a1d59bfbab7b2113292cd6f18232
Rodrigo Shinoda <rodrigo.viegas@ulbra.inf.br>
2019-06-30 21:07:31 -0300

    Models Produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    class Meta:
+        db_table = 'tb_produto'
+    
+    id = models.AutoField(primary_key=True, editable=False, auto_Created=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
